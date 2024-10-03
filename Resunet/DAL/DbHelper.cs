9dea94589b77ec7935bf2ba7096d56ad64d39b67
Gleb <annenkovgleb@proton.me>
2024-10-03 21:29:36 +0300

    registration form

---
+++ Resunet/DAL/DbHelper.cs
@@ -0,0 +1,7 @@
+namespace Resunet.DAL
+{
+    public class DbHelper
+    {
+        public static string ConnString = "User ID=postgres;Password=admin;Host=localhost;Port=5432;Database=test";
+    }
+}
