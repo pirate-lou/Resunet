c7945074fec18ed22f4d53c8b9eb23a4f245691a
Gleb <annenkovgleb@proton.me>
2024-10-13 19:22:47 +0300

    Validation

--- Resunet/ViewModels/RegisterViewModel.cs
+++ Resunet/ViewModels/RegisterViewModel.cs
@@ -1,8 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Resunet.ViewModels
 {
-    public class RegisterViewModel
+    public class RegisterViewModel : IValidatableObject
     {
+        [Required(ErrorMessage = "Без email никуда не пущу")]
+        [EmailAddress(ErrorMessage = "Формат поменяй")]
         public string? Email { get; set; }
+        
+        [Required(ErrorMessage = "Пароль обязателен")]
+        [RegularExpression("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#$%^&*-]).{10,}$",
+            ErrorMessage = "Слишком простой пароль")]
         public string? Password { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (Password == "qwerty123")
+            {
+                yield return new ValidationResult("Слишком простой пароль", new[] {"Password"});
+            }
+            
+        }
     }
 }
